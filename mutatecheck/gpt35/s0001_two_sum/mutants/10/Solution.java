c90cfbed55a9f3ada7ffa3ab510cd2276057772c
Chriss IT. Leong <itleong0713@gmail.com>
2024-04-24 22:32:58 +0800

    added generate mutant script

---
+++ mutatecheck/gpt35/s0001_two_sum/mutants/10/Solution.java
@@ -0,0 +1,29 @@
+package g0001_0100.s0001_two_sum;
+
+// #Easy #Top_100_Liked_Questions #Top_Interview_Questions #Array #Hash_Table
+// #Data_Structure_I_Day_2_Array #Level_1_Day_13_Hashmap #Udemy_Arrays #Big_O_Time_O(n)_Space_O(n)
+// #2024_01_04_Time_2_ms_(85.97%)_Space_44.8_MB_(15.45%)
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Solution {
+//@ requires(-10^9 <= target <= 10^9);
+//@ ensures((\result != null && \result.length == 2));
+//@ requires(numbers != null && numbers.length >= 2);
+//@ ensures((\exists int i, j; 0 <= i && i < numbers.length && 0 <= j && j < numbers.length && i != j; numbers[i] + numbers[j] == target));
+//@ requires((\forall int i; 0 <= i && i < numbers.length; -10^9 <= numbers[i] && numbers[i] <= 10^9));
+//@ ensures(numbers[\result[0]] + numbers[\result[1]] == target);
+//@ ensures((\forall int i; 0 <= i && i < \result.length; 0 <= \result[i] && \result[i] < numbers.length));
+    public int[] twoSum(int[] numbers, int target) {
+        Map<Integer, Integer> indexMap = new HashMap<>();
+        for (int i = 0; i < numbers.length; i++) {
+            Integer requiredNum = target - numbers[i];
+            if (indexMap.containsKey(requiredNum)) {
+                return new int[] {indexMap.get(requiredNum), i};
+            }
+
+        }
+        return new int[] {-1, -1};
+    }
+}
---
+++ scripts/createmutants.py
@@ -0,0 +1,30 @@
+from typing import Dict, List
+
+def createmutant(line: int, org: str, new: str, prog: List[str]) -> str:
+    tmp = prog
+    if new != "<NO-OP>":
+        tmp[line - 1] = tmp[line - 1].replace(org, new)
+    else:
+        tmp[line - 1] = ""
+    return '\n'.join(tmp)
+
+
+import sys
+import os
+folder = sys.argv[1]
+with open(os.path.join(folder, "mutants.log"), 'r') as fp:
+    mutantslog = fp.read()
+with open(os.path.join(folder, "Solution.java"), 'r') as fp:
+    program = fp.read()
+program = program.split('\n')
+mutantslog = mutantslog.split('\n')
+mutants = []
+import re
+for log in mutantslog:
+    if r := re.match(r'(\d+):[A-Z]+:.*\(.*,.*\):(\d+):(.*)\s+\|==>\s+(.*)', log, re.ASCII):
+        mutants.append(createmutant(int(r.group(2)), r.group(3), r.group(4), program.copy()))
+
+for i, p in enumerate(mutants):
+    os.makedirs(os.path.join(folder, "mutants", str(i)), exist_ok=True)
+    with open(os.path.join(folder, "mutants", str(i), "Solution.java"), 'w') as fp:
+        fp.write(p)
