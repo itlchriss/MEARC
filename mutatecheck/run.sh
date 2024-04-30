be860631f4b7144d2d150fadf0e1d3fb115aafd4
Chriss IT. Leong <itleong0713@gmail.com>
2024-04-30 21:35:30 +0800

    adding p3 results

---
+++ mutatecheck/run.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+OPENJML="../../openjml-0.17-a15/openjml "
+ESC_FLAG="-esc"
+BIGINT_FLAGS="--spec-math bigint --code-math bigint"
+TIMEOUT_FLAG="--timeout 60"
+
+dotest () {
+    type=$1
+    for entry in `find ./$type/*/mutants -type f -name 'Solution.java' | sort`
+    do
+        folder=$(dirname "$entry")
+        echo $entry
+        src=$(dirname $(dirname "$folder"))
+        $OPENJML $ESC_FLAG $src/Solution.java $BIGINT_FLAGS $TIMEOUT_FLAG > $src/org_result.txt
+        $OPENJML $ESC_FLAG $entry $BIGINT_FLAGS $TIMEOUT_FLAG > $folder/result.txt
+    done
+}
+
+dotest 'gpt35'
+dotest 'hart'
